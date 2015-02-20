7ca0d223e1ffbe9e147bbf17aa06da1799684b16
Mindy Preston <meetup@yomimono.org>
2015-02-20 14:17:13 +0000

    s/ocaml-nat/mirage-nat/

--- config.ml
+++ config.ml
@@ -12,8 +12,8 @@ is silently dropped in that case and we just get the first Xen network iface. *)
 let tracing = mprof_trace ~size:100000 () *)
 
 let () = 
-  add_to_opam_packages ["ocaml-nat";"tcpip";"re";"mirage-profile"];
-  add_to_ocamlfind_libraries ["mirage_nat";"re";"re.str";
+  add_to_opam_packages ["mirage-nat";"tcpip";"re";"mirage-profile"];
+  add_to_ocamlfind_libraries ["mirage-nat";"re";"re.str";
                               "tcpip.ethif";"tcpip.ipv4";"mirage-profile"];
   register "simple_nat" (* ~tracing *) [
     main $ default_console $ primary_netif $ secondary_netif 
