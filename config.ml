819e08b256fe0ca322191fa8bfa06319faf0a9db
Thomas Gazagnaire <thomas@gazagnaire.org>
2015-02-20 11:27:06 +0000

    Spaces

--- config.ml
+++ config.ml
@@ -4,16 +4,16 @@ let main = foreign "Simple_nat.Main" (console @-> network @-> network @-> job)
 
 let primary_netif = (netif "0")
 let secondary_netif = (netif "1") (* netif actually needs an integer, shoved
-into a string, which maps to a device ID number assigned by Xen, to do anything 
+into a string, which maps to a device ID number assigned by Xen, to do anything
 helpful when xen is the target.  Stuff that can't be turned into an int
 is silently dropped in that case and we just get the first Xen network iface. *)
 
 let tracing = mprof_trace ~size:100000 ()
 
-let () = 
+let () =
   add_to_opam_packages ["ocaml-nat";"tcpip";"mirage-profile"];
   add_to_ocamlfind_libraries ["mirage_nat";
                               "tcpip.ethif";"tcpip.ipv4";"mirage-profile"];
   register "simple_nat" ~tracing [
-    main $ default_console $ primary_netif $ secondary_netif 
+    main $ default_console $ primary_netif $ secondary_netif
   ]
