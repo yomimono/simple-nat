63bcb3c531a8a6a4ca19f668a32da5751f697c13
Mindy Preston <meetup@yomimono.org>
2015-08-10 14:48:51 +0100

    add a different, more modern example of a Xen config for the NAT device

--- nat_setup.sh
+++
@@ -1,1 +0,0 @@
-sudo xl create simple_nat.xl -c 'extra="external_ip=192.168.3.99 external_netmask=255.255.255.0 external_gateway=192.168.3.1 internal_ip=192.168.252.2 internal_netmask=255.255.255.0"'
