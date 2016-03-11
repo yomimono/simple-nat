3e5e5fc56d3f145d1e28b2f35c4d0795a9f509f9
Mindy Preston <mindy.preston@docker.com>
2016-03-11 14:23:46 +0000

    require mirage-nat.hashtable

--- config.ml
+++ config.ml
@@ -12,7 +12,7 @@ let tracing = mprof_trace ~size:100000 () *)
 
 let () =
   add_to_opam_packages ["mirage-nat";"re";"tcpip";"mirage-profile"];
-  add_to_ocamlfind_libraries ["mirage-nat";"re.str";"tcpip.arpv4";
+  add_to_ocamlfind_libraries ["mirage-nat";"mirage-nat.hashtable";"re.str";"tcpip.arpv4";
                               "tcpip.ethif";"tcpip.ipv4";"mirage-profile"];
   register "simple-nat" (* ~tracing *) [
     main $ default_console $ default_clock $ default_time $ primary_netif $ secondary_netif
