df0ab1446a258285fea0ee4f808ddac6e744ca7e
Mindy Preston <mindy.preston@docker.com>
2016-03-11 14:23:34 +0000

    now with it-works

--- simple_nat.ml
+++ simple_nat.ml
@@ -1,14 +1,20 @@
 open V1_LWT
 open Lwt
-open Nat_rewrite
 
 module Main (C: CONSOLE) (Clock: V1.CLOCK) (Time: V1_LWT.TIME)
-  (PRI: NETWORK) (SEC: NETWORK) = struct
+    (PRI: NETWORK) (SEC: NETWORK) = struct
+
+  module Nat_clock = struct
+    let now () = Clock.time () |> Int64.of_float
+  end
+
+module Nat_rewrite = Mirage_nat_hashtable.Make(Nat_clock)(Time)
 
 module ETH = Ethif.Make(PRI)
   module A = Arpv4.Make(ETH)(Clock)(Time)
   module I = Ipv4.Make(ETH)(A)
-  type direction = Nat_rewrite.direction
+
+  type direction = | Source | Destination
 
   let listen nf a push =
     (* ingest packets *)
@@ -25,9 +31,10 @@ module ETH = Ethif.Make(PRI)
       | n when n < 1024 ->
         stubborn_insert table frame ip (Random.int 65535)
       | n ->
-        match Nat_rewrite.make_nat_entry table frame ip n with
-        | Ok t -> Some t
-        | Unparseable -> None
+        let open Nat_rewrite in
+        add_nat table frame (ip, n) >>= function
+        | Ok -> Lwt.return (Some ())
+        | Unparseable -> Lwt.return None
         | Overlap -> stubborn_insert table frame ip (Random.int 65535)
     in
     (* TODO: connection tracking logic *)
@@ -36,18 +43,20 @@ module ETH = Ethif.Make(PRI)
   let nat external_ip internal_ip nat_table (direction : direction)
       in_queue out_push =
     let rec frame_wrapper frame =
+      let open Mirage_nat in
       (* typical NAT logic: traffic from the internal "trusted" interface gets
          new mappings by default; traffic from other interfaces gets dropped if
          no mapping exists (which it doesn't, since we already checked) *)
-      match direction, (Nat_rewrite.translate nat_table direction frame) with
-      | Destination, None -> 
+      Nat_rewrite.translate nat_table frame >>= fun f ->
+      match direction, f with
+      | Destination, Untranslated -> 
         Lwt.return_unit (* nothing in the table, drop it *)
-      | _, Some f ->
-        return (out_push (Some f))
-      | Source, None ->
+      | _, Translated ->
+        return (out_push (Some frame))
+      | Source, Untranslated ->
         (* mutate nat_table to include entries for the frame *)
-        match allow_nat_traffic nat_table frame external_ip with
-        | Some t ->
+        allow_nat_traffic nat_table frame external_ip >>= function
+        | Some () ->
           (* try rewriting again; we should now have an entry for this packet *)
           frame_wrapper frame
         | None ->
@@ -58,66 +67,63 @@ module ETH = Ethif.Make(PRI)
       Lwt_stream.next in_queue >>= frame_wrapper
     done
 
-let send_packets c nf i out_queue =
+let send_packets c nf i (out_queue : Cstruct.t Lwt_stream.t) =
   while_lwt true do
     lwt frame = Lwt_stream.next out_queue in
 
-    match Nat_rewrite.layers frame with
+    match Nat_decompose.layers frame with
     | None -> raise (Invalid_argument "NAT transformation rendered packet unparseable")
-    | Some (ether, ip, tx) ->
-      let ether = Nat_rewrite.set_smac ether (PRI.mac nf) in
+    | Some (ether, ip, tx, payload) ->
+      let ether = Nat_decompose.set_smac ether (PRI.mac nf) in
       let (just_headers, higherlevel_data) =
-        Nat_rewrite.recalculate_transport_checksum (I.checksum) (ether, ip, tx)
+        Nat_decompose.finalize_packet (ether, ip, tx, payload)
       in
       I.writev i just_headers [ higherlevel_data ] >>= fun () -> return_unit
   done
 
 let start c _clock _time pri sec =
 
-  let (pri_in_queue, pri_in_push) = Lwt_stream.create () in
-  let (pri_out_queue, pri_out_push) = Lwt_stream.create () in
-  let (sec_in_queue, sec_in_push) = Lwt_stream.create () in
-  let (sec_out_queue, sec_out_push) = Lwt_stream.create () in
+let (pri_in_queue, pri_in_push) = Lwt_stream.create () in
+let (pri_out_queue, pri_out_push) = Lwt_stream.create () in
+let (sec_in_queue, sec_in_push) = Lwt_stream.create () in
+let (sec_out_queue, sec_out_push) = Lwt_stream.create () in
 
-  (* or_error brazenly stolen from netif-forward *)
-  let or_error c name fn t =
-    fn t
-    >>= function
-    | `Error e -> fail (Failure ("error starting " ^ name))
-    | `Ok t -> C.log_s c (Printf.sprintf "%s connected." name) >>
-      return t
-  in
+(* or_error brazenly stolen from netif-forward *)
+let or_error c name fn t =
+  fn t
+  >>= function
+  | `Error e -> fail (Failure ("error starting " ^ name))
+  | `Ok t -> C.log_s c (Printf.sprintf "%s connected." name) >>
+    return t
+in
 
-  (* get network configuration from bootvars *)
-  Bootvar.create >>= fun bootvar ->
-  let try_bootvar key = Ipaddr.V4.of_string_exn (Bootvar.get bootvar key) in
-  let internal_ip = try_bootvar "internal_ip" in
-  let internal_netmask = try_bootvar "internal_netmask" in
-  let external_ip = try_bootvar "external_ip" in
-  let external_netmask = try_bootvar "external_netmask" in
-  let external_gateway = try_bootvar "external_gateway" in
+(* get network configuration from bootvars *)
+Bootvar.create >>= fun bootvar ->
+let try_bootvar key = Ipaddr.V4.of_string_exn (Bootvar.get bootvar key) in
+let internal_ip = try_bootvar "internal_ip" in
+let internal_netmask = try_bootvar "internal_netmask" in
+let external_ip = try_bootvar "external_ip" in
+let external_netmask = try_bootvar "external_netmask" in
+let external_gateway = try_bootvar "external_gateway" in
 
-  (* initialize interfaces *)
-  lwt nf1 = or_error c "primary interface" ETH.connect pri in
-  lwt nf2 = or_error c "secondary interface" ETH.connect sec in
+(* initialize interfaces *)
+lwt nf1 = or_error c "primary interface" ETH.connect pri in
+lwt nf2 = or_error c "secondary interface" ETH.connect sec in
 
-  lwt arp1 = or_error c "primary arp" A.connect nf1 in
-  lwt arp2 = or_error c "primary arp" A.connect nf2 in
+lwt arp1 = or_error c "primary arp" A.connect nf1 in
+lwt arp2 = or_error c "primary arp" A.connect nf2 in
 
-  (* set up ipv4 on interfaces so ARP will be answered *)
-  lwt ext_i = or_error c "ip for primary interface" (I.connect nf1) arp1 in
-  lwt int_i = or_error c "ip for secondary interface" (I.connect nf2) arp2 in
-  I.set_ip ext_i external_ip >>= fun () ->
-  I.set_ip_netmask ext_i external_netmask >>= fun () ->
-  I.set_ip int_i internal_ip >>= fun () ->
-  I.set_ip_netmask int_i internal_netmask >>= fun () ->
-  I.set_ip_gateways ext_i [ external_gateway ] >>= fun () -> ();
+(* set up ipv4 on interfaces so ARP will be answered *)
+lwt ext_i = or_error c "ip for primary interface" (I.connect nf1) arp1 in
+lwt int_i = or_error c "ip for secondary interface" (I.connect nf2) arp2 in
+I.set_ip ext_i external_ip >>= fun () ->
+I.set_ip_netmask ext_i external_netmask >>= fun () ->
+I.set_ip int_i internal_ip >>= fun () ->
+I.set_ip_netmask int_i internal_netmask >>= fun () ->
+I.set_ip_gateways ext_i [ external_gateway ] >>= fun () -> ();
 
-  (* TODO: provide hooks for updates to/dump of this *)
-  let table () = Nat_lookup.empty ()
-  in
-
-  let nat_t = table () in
+(* TODO: provide hooks for updates to/dump of this *)
+Nat_rewrite.empty () >>= fun nat_t ->
 
   Lwt.choose [
     (* packet intake *)
